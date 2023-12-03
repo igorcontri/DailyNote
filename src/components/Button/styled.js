7110223ca5cbeaefe223691fd001e715070c6e96
Igor Contri <igorcontri@gmail.com>
2023-12-03 16:08:08 -0300

    Fixing Buttons

---
+++ src/components/Button/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+    width: 100%;
+    background-color: ${ ({ theme }) => theme.COLORS.ORANGE};
+    color: ${ ({ theme }) => theme.COLORS.BACKGROUN_800 };
+
+    height: 3.5rem;
+    border: 0;
+    padding: 0 1rem;
+    margin-top: 1rem;
+    border-radius: .6rem;
+    font-weight: 500;
+
+    /* button:disabled { opacity: .5; } */
+    &:disabled {
+        opacity: .5;
+
+    }
+`;
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,17 @@
-import { Container } from './styles.js'
+import { Container } from './styles.js';
+
+import { Button } from '../../components/Button/index.jsx';
 
 export function Details() {
-  
+
   return (
     <Container>
       <h1>Hello World!</h1>
       <p>component</p>
+      <Button title="Sign In"/>
+      <Button title="Sign Up"/>
+      <Button title="Return"/>
+
     </Container>
   )
 }
