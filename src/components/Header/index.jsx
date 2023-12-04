dbf03e02b84703f4a8e87483b08ddd42fed8f662
Igor Contri <igorcontri@gmail.com>
2023-12-04 17:11:04 -0300

    Mini profile  + favicon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,18 @@
-import { Container } from './styles';
+import { Container, Profile } from './styles';
 
 export function Header() {
     return(
         <Container>
-            
+            <Profile>
+                <img
+                    src="https://github.com/igorcontri.png"
+                />
+
+                <div>
+                    <span>Welcome</span>
+                    <strong>Igor Contri</strong>
+                </div>
+            </Profile>
         </Container>
     );
 }
