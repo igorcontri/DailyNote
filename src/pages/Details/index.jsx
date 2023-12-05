e1d91977d02945ff27bba8021d021a92f05dda3f
Igor Contri <igorcontri@gmail.com>
2023-12-05 14:51:08 -0300

    Tag Component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from './styles.js';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 
 export function Details() {
 
@@ -14,6 +15,13 @@ export function Details() {
           <li><a href="https://www.google.com" target="_blank">google.com</a></li>
           <li><a href="https://www.google.com" target="_blank">google.com</a></li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+       <Tag title="Hello," />
+       <Tag title="my" />
+       <Tag title="name" />
+       <Tag title="is" />
       </Section>
       
       <Button title="Return" />
