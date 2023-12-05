233b182af2b4a7f7558dc95673f0a8bac681bc9e
Igor Contri <igorcontri@gmail.com>
2023-12-05 13:55:05 -0300

    Section component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,21 @@
-import { Container } from './styles.js';
+import { Container, Links } from './styles.js';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
 
 export function Details() {
 
   return (
     <Container>
       <Header />
+      
+      <Section title="Links">
+        <Links>
+          <li><a href="https://www.google.com" target="_blank">google.com</a></li>
+          <li><a href="https://www.google.com" target="_blank">google.com</a></li>
+        </Links>
+      </Section>
+      
       <Button title="Return" />
     </Container>
   )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -10,4 +10,16 @@ export const Container = styled.div`
 
 `;
 
+export const Links = styled.ul`
+    list-style: none;
+
+    > li {
+        margin-top: 12px;
+
+        a {
+            color: ${({ theme }) => theme.COLORS.WHITE} ;
+        }
+    }
+`;
+
 
