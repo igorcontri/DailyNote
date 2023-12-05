71cff9188f269f5c7e17b11e175abdc811ddf2a6
Igor Contri <igorcontri@gmail.com>
2023-12-05 15:46:45 -0300

    Page Content

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,6 @@
-import { Container, Links } from './styles.js';
+import { Container, Links, Content } from './styles.js';
 import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
@@ -10,21 +11,36 @@ export function Details() {
     <Container>
       <Header />
       
-      <Section title="Links">
-        <Links>
-          <li><a href="https://www.google.com" target="_blank">google.com</a></li>
-          <li><a href="https://www.google.com" target="_blank">google.com</a></li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
 
-      <Section title="Marcadores">
-       <Tag title="Hello," />
-       <Tag title="my" />
-       <Tag title="name" />
-       <Tag title="is" />
-      </Section>
-      
-      <Button title="Return" />
+        
+          <ButtonText title="Clear Page" />
+
+            <h1> Learning React </h1>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci placeat necessitatibus eveniet tenetur voluptatibus magnam et harum? Ipsam obcaecati eos sapiente laudantium minima vitae necessitatibus officia delectus tempore. Minima, debitis.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci placeat necessitatibus eveniet tenetur voluptatibus magnam et harum? Ipsam obcaecati eos sapiente laudantium minima vitae necessitatibus officia delectus tempore. Minima, debitis.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci placeat necessitatibus eveniet tenetur voluptatibus magnam et harum? Ipsam obcaecati eos sapiente laudantium minima vitae necessitatibus officia delectus tempore. Minima, debitis.
+            </p>
+
+          <Section title="Links">
+            <Links>
+              <li><a href="https://www.google.com" target="_blank">google.com</a></li>
+              <li><a href="https://www.google.com" target="_blank">google.com</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="Lorem" />
+            <Tag title="Ipsum" />
+            <Tag title="Dolor" />
+          </Section>
+          
+          <Button title="Return" />
+        </Content>
+      </main>
     </Container>
   )
 }
