b8eabf411b2d17a86f70a2f1f8c7f72be18b0455
Igor Contri <igorcontri@gmail.com>
2023-12-08 11:24:56 -0300

    app title

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-    margin: 56px 0 28px;
+    margin: 56px 0 0;
 
     > h2 {
         border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUN_700};
 
         padding-bottom: 16px;
-        
+        margin-bottom: 28px;
+
         color: ${({ theme }) => theme.COLORS.GRAY_100};
         font-size: 1.25rem;
         font-weight: 400;
