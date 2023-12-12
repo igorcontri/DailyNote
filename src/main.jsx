1a956a9f6e3dba065bfb33c54b563451bf78b735
Igor Contri <igorcontri@gmail.com>
2023-12-12 18:29:33 -0300

    SignIn Page

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,16 @@ import GlobalStyles from "./styles/global.js";
 import theme from "./styles/theme.js";
 
 // import { Details } from './pages/Details/index.jsx'
-import { Home } from "./pages/Home/index.jsx";
+// import { Home } from "./pages/Home/index.jsx";
+import { SignIn } from "./pages/SignIn/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <GlobalStyles />
             {/* <Details /> */}
-            <Home />
+            {/* <Home /> */}
+            <SignIn />
         </ThemeProvider>
     </React.StrictMode>
 );
