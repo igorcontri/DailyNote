01963d6ecf99036111e125568904cac72ab20da7
Igor Contri <igorcontri@gmail.com>
2023-12-12 19:59:18 -0300

    Profile Page

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global.js";
 import theme from "./styles/theme.js";
 
-// import { Details } from './pages/Details/index.jsx'
+// import { Details } from "./pages/Details/index.jsx";
 // import { Home } from "./pages/Home/index.jsx";
 // import { SignIn } from "./pages/SignIn/index.jsx";
-import { SignUp } from "./pages/SignUp/index.jsx";
+// import { SignUp } from "./pages/SignUp/index.jsx";
+import { Profile } from "./pages/Profile/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
@@ -16,7 +17,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             {/* <Details /> */}
             {/* <Home /> */}
             {/* <SignIn /> */}
-            <SignUp />
+            {/* <SignUp /> */}
+            <Profile />
         </ThemeProvider>
     </React.StrictMode>
 );
